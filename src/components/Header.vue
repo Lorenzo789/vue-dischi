4978aefc548c8bd2031ccc75ae24bdc829022315
Lorenzo789 <pallozzilorenzo@gmail.com>
2022-07-19 15:18:36 +0200

    create new components and impor boostrap-icons

---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <header>
+    <h1>Header</h1>
+    <i class="bi bi-spotify"></i>
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+    @import'../styles/variables.scss';
+    @import'../../node_modules/bootstrap-icons/font/bootstrap-icons.css';
+</style>
